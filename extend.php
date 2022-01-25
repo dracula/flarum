da20627a711d6a8d2c75908ab0091bf95d6d3c16
Darkle <darkle@protonmail.com>
2022-01-25 20:01:52 +0100

    Update extend.php

--- extend.php
+++ extend.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of iamdarkle/fancybox
+ *
+ * Copyright (c) 2021 Tomás Romero.
+ *
+ * For the full copyright and license information, please view the LICENSE.md
+ * file that was distributed with this source code.
+ */
+
 namespace dracula\theme;
 
 use Flarum\Extend;
